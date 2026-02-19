9923f277814f2be6cfdad3e3ddf2df3b00557fe2
Akhil-Tripathi01 <akhil.tripathi_cs.aiml24@gla.ac.in>
2026-02-19 15:25:32 +0530

    implement IR sensor functionality and initialization

--- src/main.cpp
+++ src/main.cpp
@@ -2,43 +2,31 @@
 /**
  * @file main.cpp
  * @brief Embedded Obstacle Detection System using IR Sensor
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @author Akhil
+ * @date 2026-02-19
  *
  * @details
  * Reads digital input from IR sensor
  * and detects obstacle presence.
  */
 
- // TODO 1:
- // Define IR sensor digital pin (Use pin 2)
-
- // TODO 2:
- // Create variable to store sensor state
+const int IR_PIN = 2;
+int sensorState = 0;
 
 void setup() {
-
-    // TODO 3:
-    // Initialize Serial communication (9600 baud rate)
-
-    // TODO 4:
-    // Configure IR pin as INPUT
-
-    // TODO 5:
-    // Print system initialization message
+    Serial.begin(9600);
+    pinMode(IR_PIN, INPUT);
+    Serial.println("System Initialized - IR Sensor Ready");
 }
 
 void loop() {
+    sensorState = digitalRead(IR_PIN);
 
-    // TODO 6:
-    // Read digital value from IR sensor
+    if (sensorState == HIGH) {
+        Serial.println("Obstacle Detected");
+    } else {
+        Serial.println("No Obstacle");
+    }
 
-    // TODO 7:
-    // If obstacle detected
-    //     Print "Obstacle Detected"
-    // Else
-    //     Print "No Obstacle"
-
-    // TODO 8:
-    // Add small delay (200–500ms)
+    delay(300);
 }
